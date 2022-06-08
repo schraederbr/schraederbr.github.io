8820a52b19b5d6984b9f57c26c843558d75dabdb
Ben Schraeder <schraeder.br@gmail.com>
2022-06-08 13:41:57 -0600

    Imported more packages

---
+++ index.py
@@ -0,0 +1,8 @@
+import requests
+import spotipy
+# print("Imported Requests")
+# url = 'https://jsonplaceholder.typicode.com/posts'
+# response = requests.request("GET", url)
+#print(response.text)
+
+
